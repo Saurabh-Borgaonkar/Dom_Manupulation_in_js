9e86052dec96d83af480dfa99f5f1a2ef5eb41b6
Saurabh-Borgaonkar <saurabhborgaonkar55@gmail.com>
2026-06-25 20:16:56 +0530

    String methods

---
+++ String/string.js
@@ -0,0 +1,27 @@
+
+let name="xbdgdyehrggeg";
+console.log(name.length);
+
+//touppercase 
+
+let name1="SAURABH";
+console.log(name1.toLocaleLowerCase());
+
+let s="      helooo    ";
+console.log(s.trim());
+
+let str="Aman is a girl";
+console.log(str.startsWith("Aman"));
+
+let str1="Aman is a girl";
+console.log(str1.endsWith("Aman"));
+
+let str3="Saurabh Bhaiii";
+console.log(str3.includes("Bhaiii"));
+
+let str4="qwerty hiii";
+console.log(str4.slice(2,5));
+console.log(str4.split(" "));
+console.log(str4);
+
+
